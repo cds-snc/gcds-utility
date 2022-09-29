bb4034e2c5f792e50ff129b1d8adc7068a0f012d
Melanie Boeckmann <melanie.bockmann@gmail.com>
2022-09-29 13:39:21 -0700

    add basic set up + classes for utility framework

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,33 @@
-'use strict';
+const { dest, series, src, watch } = require('gulp');
+const minify = require('gulp-clean-css');
+const prefix = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass')(require('sass'));
 
-const { src, dest } = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
-
+// Compile + prefix SCSS
 function compile() {
-  return src('src/utilities.scss').pipe(sass.sync()).pipe(dest('dist'));
+  return src('src/utilities.scss')
+    .pipe(sass())
+    .pipe(prefix('last 2 versions'))
+    .pipe(dest('dist'))
+};
+
+// Minify CSS
+function minifyCss() {
+  return src('dist/utilities.css')
+    .pipe(minify())
+    .pipe(rename('utilities.min.css'))
+    .pipe(dest('dist'))
+};
+
+// Watch SCSS for changes
+function watchTask() {
+  watch('src/*.scss', compile);
 }
 
-module.exports = {
+// Default gulp
+exports.default = series(
   compile,
-};
+  minifyCss,
+  watchTask
+);
